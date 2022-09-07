2e9d7cfa3bdf46c2483a6e5abaaace775dd99f50
davethecodeslayer <davidjoseml12@gmail.com>
2022-09-07 23:11:35 +0000

    logica creada sufriendo con el estilo

--- src/app.js
+++ src/app.js
@@ -7,5 +7,47 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  var pints = ["♦", "♥", "♠", "♣"];
+  var numbers = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "Q",
+    "K",
+    "J",
+    "A"
+  ];
+
+  let getPint = Math.floor(Math.random() * 4);
+  let getNumber = Math.floor(Math.random() * 12);
+
+  let randomPint = pints[getPint];
+  let randomNumber = numbers[getNumber];
+
+  let callingCardNumber = document.querySelector(".cardNumber");
+  callingCardNumber.innerText = randomNumber;
+
+  let callingCardPints = document.querySelectorAll(".cardPint");
+  callingCardPints.forEach(callingCardPint => {
+    callingCardPint.innerText = randomPint;
+  });
+
+  let callingCard = document.querySelector(".Card");
+
+  if (randomPint === "♦") {
+    callingCard.classList.add("diamond");
+  } else if (randomPint === "♥") {
+    callingCard.classList.add("heart");
+  } else if (randomPint == "♠") {
+    callingCard.classList.add("spade");
+  } else {
+    callingCard.classList.add("club");
+  }
 };
